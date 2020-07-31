665bbcb7ea20b7db7be5f1dc9fe7263adef3c5c6
John Madrigal <johnmadrigal17@gmail.com>
2020-07-31 16:58:46 -0700

    make post request to db via postman

--- server/controllers/reminderController.js
+++ server/controllers/reminderController.js
@@ -1,1 +1,17 @@
-const Reminder = require('')
+const Reminder = require('../models/model.js');
+
+const reminderController = {};
+
+reminderController.postReminder = async (req, res, next) => {
+  const { text } = req.body
+  try {
+    const reminder = await Reminder.create({text})
+    res.locals.data = reminder;
+    return next();
+  } catch (e) {
+    return next(err)
+  }
+
+}
+
+module.exports = reminderController;
--- server/server.js
+++ server/server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const Reminder = require('./models/model.js')
+const reminderController = require('./controllers/reminderController.js')
 
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.use('/assets/static', express.static(path.join(__dirname, '../assets/static')))
 app.get('/', (req,res) => {
-  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+  return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.post('/reminder')
+app.post('/reminder', reminderController.postReminder, (req, res) => {
+  return res.status(200).json(res.locals.data);
+})
 
 app.use('*', (req, res) => {
   res.status(404);
