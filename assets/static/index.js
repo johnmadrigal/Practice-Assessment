39d8d74aa8af9c8a83ca72c4a6ac202854692023
John Madrigal <johnmadrigal17@gmail.com>
2020-07-31 19:09:23 -0700

    post request to db from frontend

--- assets/static/index.js
+++ assets/static/index.js
@@ -1,26 +1,17 @@
-
 console.log("hello")
 
-function getReminders() {
+function getReminders(e) {
   //select ul tag
   const ul = document.getElementById('reminders')
+  ul.innerHTML = '';
   //make a call to the server for reminders
   fetch('/reminder')
     .then( res => res.json())
     .then( reminders => {
       // console.log('data', data)
       reminders.forEach( rem => {
-        //create an li tag
-        const li = document.createElement('li');
-        //set innerHTML to reminder
-        li.id = rem._id
-        li.innerHTML= rem.text;
-        const button = document.createElement('button')
-        button.type = 'submit';
-        button.innerHTML = 'delete';
-        li.append(button);
-        //append li tag to ul
-        ul.append(li);
+        const reminder = createReminder(rem)
+        ul.append(reminder);
       })
     })
     .catch( err => console.log('err', err))
@@ -30,4 +21,49 @@ function getReminders() {
   //
 }
 
+function createReminder(reminder) {
+ //create an li tag
+ const li = document.createElement('li');
+ //set innerHTML to reminder
+ li.id = reminder._id
+ li.innerHTML= reminder.text;
+ const button = document.createElement('button')
+ button.type = 'submit';
+ button.innerHTML = 'delete';
+ button.addEventListener('submit', (e) => {
+   e.preventDefault();
+   
+ })
+ li.append(button);
+ //append li tag to ul
+ return li
+}
+
 getReminders();
+
+//refresh getreminders
+document.getElementById('reset').addEventListener('click', getReminders)
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formText = document.getElementById('text')
+  // console.log(formText.value)
+  const body = {
+    text: formText.value
+  }
+  fetch('/reminder', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  .then( res => res.json)
+  .then( reminder => 
+    {
+      formText.value = ''
+    })
+  .catch( err => console.log(err))
+})
